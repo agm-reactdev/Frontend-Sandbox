79d7cdbfb8c135767c3515ecbf65c06a6f72d3f6
agm-reactdev <abdulghaffarmemon052@gmail.com>
2026-08-27 10:01:56 +0500

    notification

--- src/Components/Youtubenotificationpopup/Youtubenotificationtoggle.jsx
+++ src/Components/Youtubenotificationpopup/Youtubenotificationtoggle.jsx
@@ -2,20 +2,30 @@ import { useState } from "react";
 import { Bell } from "lucide-react";
 import Youtubepopup from "./Youtubepopup";
 
+function Youtubenotificationtoggle() {
+  const [open, setOpen] = useState(false);
+  const [count, setCount] = useState(1);
 
-function Youtubenotificationtoggle () {
+  function isopen() {
+    setOpen(!open);
+    setCount(count === 1 ? null : count + 1);
+  }
+  return (
+    <div className="flex flex-col items-center  mt-2">
+      <span className="bg-red-500 text-white px-2 rounded-full border relative top-5 left-3">
+        {count}
+      </span>
 
-    const [ open , setOpen] = useState(false);
-    
-    function isopen () {
-        setOpen(!open);
-    }
-    return (
-         <div className="flex flex-col items-center  mt-2">
-            <button  className="p-2 rounded-full bg-white text-black hover:bg-gray-600 hover:text-white" onClick={isopen}><Bell/></button>
-            {open ? <Youtubepopup/> : null}
-         </div>
-    )
+      <button
+        className="p-2 rounded-full bg-white text-black "
+        onClick={isopen}
+      >
+        <Bell />{" "}
+      </button>
+
+      <div className="mt-10">{open ? <Youtubepopup /> : null}</div>
+    </div>
+  );
 }
 
 export default Youtubenotificationtoggle;
